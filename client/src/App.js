4f214744981fc9bc881c2e350bebf30a183b07f0
ozcod <ozair.isb@gmail.com>
2023-01-11 15:57:18 +0100

    blog-post

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import Update from "./pages/Update";
+import Books from "./pages/Books";
+import Add from "./pages/Add";
+import "./style.css";
+
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update/:id" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Add.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Add = () => {
+  const [book, setBooks] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setBooks((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8800/books", book);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  console.log(book);
+  return (
+    <div className="form">
+      <h1>Add new book</h1>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        name="title"
+      ></input>
+      <input
+        type="text"
+        placeholder="desc"
+        onChange={handleChange}
+        name="desc"
+      ></input>
+
+      <input
+        type="text"
+        placeholder="cover"
+        onChange={handleChange}
+        name="cover"
+      ></input>
+      <button className="formButton" onClick={handleClick}>
+        Add
+      </button>
+    </div>
+  );
+};
+
+export default Add;
